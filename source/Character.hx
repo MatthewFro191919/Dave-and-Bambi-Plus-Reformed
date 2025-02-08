6c48cc82e6573e181670ff66173a51fada78c110
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-02-08 06:15:44 -0500

    Update Character.hx

--- source/Character.hx
+++ source/Character.hx
@@ -85,6 +85,8 @@ class Character extends FlxSprite
 
 				nativelyPlayable = true;
 
+				globalOffset = [10, 210];
+
 				flipX = true;
 			case 'bf-3d':
 				frames = Paths.getSparrowAtlas('characters/3d_bf', 'shared');
